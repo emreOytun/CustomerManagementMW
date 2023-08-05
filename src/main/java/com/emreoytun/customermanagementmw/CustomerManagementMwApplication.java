260c676a6fa0761858d2cbbc17d64f22baaab16c
emreOytun <emreoytun58@gmail.com>
2023-08-06 00:40:18 +0300

    Data dependencies removed

--- src/main/java/com/emreoytun/customermanagementmw/CustomerManagementMwApplication.java
+++ src/main/java/com/emreoytun/customermanagementmw/CustomerManagementMwApplication.java
@@ -3,15 +3,12 @@ package com.emreoytun.customermanagementmw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("com.emreoytun.customermanagementdata.entities")
 @ComponentScan(basePackages = "com.emreoytun.customermanagementdata")
 @ComponentScan(basePackages = "com.emreoytun.customermanagementmw")
-@EnableJpaRepositories("com.emreoytun.customermanagementdata.repository")
 //@EnableDiscoveryClient
 public class CustomerManagementMwApplication {
 
